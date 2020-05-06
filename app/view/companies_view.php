51552d976bb49e049629f53c438bf90aeae3bf4d
Jana <janakhamdy@gmail.com>
2020-05-07 01:28:46 +0200

    added project
    Edit and View Company are in the same file as comapnies list

--- app/view/companies_view.php
+++ app/view/companies_view.php
@@ -7,36 +7,33 @@ class CompaniesView extends View
     {
         $str = "";
 
-        $str.=' <div class="table-responsive"> <table class="table"> ';
-        $str.=' <thead class=" text-primary"> ';
-        $str.=' <th> Comapny </th> ';  
-        $str.=' <th> Email </th> ';
-        $str.=' <th> Phone Number </th> ';
-        $str.=' <th> Website </th> ';
-        $str.=' <th> Address </th> ';
-        $str.=' <th class="text-right"> </th> ';
-        $str.=' </thead> <tbody> ';
+        $str .= ' <div class="table-responsive"> <table class="table"> ';
+        $str .= ' <thead class=" text-primary"> ';
+        $str .= ' <th> Comapny </th> ';
+        $str .= ' <th> Email </th> ';
+        $str .= ' <th> Phone Number </th> ';
+        $str .= ' <th> Website </th> ';
+        $str .= ' <th> Address </th> ';
+        $str .= ' <th class="text-right"> </th> ';
+        $str .= ' </thead> <tbody> ';
 
-        foreach($this->model->getComapnies() as $Company)
-        {
-            $str.=' <tr>';
-            $str.=' <td>  '.$Company->getName().'  </td> ';
-            $str.=' <td>  '.$Company->getEmail().'  </td> ';
-            $str.=' <td>  '.$Company->getPhoneNumber().'  </td> ';
-            $str.=' <td>  '.$Company->getURL().'  </td> ';
-            $str.=' <td>  '.$Company->getAddress().'  </td> ';
-            $str.=' <td class="text-right"><a class="btn" href="editCompany.php"> Edit </a></td> ';   //check href
-            $str.=' </tr> ';
+        foreach ($this->model->getComapnies() as $Company) {
+            $str .= ' <tr>';
+            $str .= ' <td>  ' . $Company->getName() . '  </td> ';
+            $str .= ' <td>  ' . $Company->getEmail() . '  </td> ';
+            $str .= ' <td>  ' . $Company->getPhoneNumber() . '  </td> ';
+            $str .= ' <td>  ' . $Company->getURL() . '  </td> ';
+            $str .= ' <td>  ' . $Company->getAddress() . '  </td> ';
+            $str .= ' <td class="text-right"><a class="btn" href="#" onclick="document.getElementById(\'id02\').style.display=\'block\' ,document.getElementById(\'id01\').style.display=\'none\'" > Edit </a></td> ';   //check href
+            $str .= ' <td class="text-right"><a class="btn" href="#" onclick="document.getElementById(\'id03\').style.display=\'block\' ,document.getElementById(\'id01\').style.display=\'none\'" > View </a></td> ';   //check href
+            $str .= ' </tr> ';
         }
 
-        $str.=' </tbody> ';
-        $str.=' </table> ';
-        $str.=' <a class="btn" href="addCompany.php"> Add Company </a> '; //ADD COMPANY ACTION HERE
-        $str.=' </div> ';
-  
+        $str .= ' </tbody> ';
+        $str .= ' </table> ';
+        $str .= ' <a class="btn" href="addCompany.php"> Add Company </a> '; //ADD COMPANY ACTION HERE
+        $str .= ' </div> ';
+
         return $str;
     }
-
-}    
-?>
-
+}
--- public/companiesList.php
+++ public/companiesList.php
@@ -1,14 +1,21 @@
-<?php  
+<?php
 
-define('__ROOT__', "../app/"); 
+define('__ROOT__', "../app/");
 require_once(__ROOT__ . "model/company_model.php");
 require_once(__ROOT__ . "model/companies_model.php");
 require_once(__ROOT__ . "controller/company_controller.php");
 require_once(__ROOT__ . "view/companies_view.php");
+require_once(__ROOT__ . "view/company_view.php");
 
+
+$company_id = 1;
 $model = new Companies();
 $controller = new CompanyController($model);
 $view = new CompaniesView($controller, $model);
+
+$model2 = new Company($company_id);
+$controller2 = new CompanyController($model2);
+$view2 = new companyView($controller2, $model2);
 
 
 /*
@@ -41,11 +48,17 @@ if (isset($_GET['action']) && !empty($_GET['action']))
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
-        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <!-- CSS Files -->
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="../assets/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
+    <style>
+        .modal {
+            display: none;
+
+        }
+    </style>
+
 </head>
 
 <body class="">
@@ -59,19 +72,28 @@ if (isset($_GET['action']) && !empty($_GET['action']))
             <div class="sidebar-wrapper" id="sidebar-wrapper">
                 <ul class="nav">
                     <li>
-                        <a href=" "> <i class="now-ui-icons design_app"> </i> <p>Dashboard</p></a> 
+                        <a href=" "> <i class="now-ui-icons design_app"> </i>
+                            <p>Dashboard</p>
+                        </a>
                     </li>
                     <li>
-                        <a href=" "> <i class="now-ui-icons users_single-02"></i><p>Employees</p></a>
+                        <a href=" "> <i class="now-ui-icons users_single-02"></i>
+                            <p>Employees</p>
+                        </a>
                     </li>
-                    <li class="active "> 
-                        < <a href="companiesList.php"> <i class="now-ui-icons files_box"></i> <p>Companies</p></a> 
-                    </li>
-                    <li> 
-                        <a href=" "> <i class="now-ui-icons files_box"></i> <p>Shipping Lines</p></a> 
+                    <li class="active ">
+                        < <a href="companiesList.php"> <i class="now-ui-icons files_box"></i>
+                            <p>Companies</p></a>
                     </li>
                     <li>
-                        <a href=" "><i class="now-ui-icons files_box"></i><p>Contracts</p></a>                              
+                        <a href=" "> <i class="now-ui-icons files_box"></i>
+                            <p>Shipping Lines</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href=" "><i class="now-ui-icons files_box"></i>
+                            <p>Contracts</p>
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -89,7 +111,7 @@ if (isset($_GET['action']) && !empty($_GET['action']))
                                 <span class="navbar-toggler-bar bar3"></span>
                             </button>
                         </div>
-                        <a class="navbar-brand" href="#pablo">  </a>
+                        <a class="navbar-brand" href="#pablo"> </a>
                     </div>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-bar navbar-kebab"></span>
@@ -101,7 +123,7 @@ if (isset($_GET['action']) && !empty($_GET['action']))
                             <div class="input-group no-border">
                                 <input type="text" value="" class="form-control" placeholder="Search...">
                                 <div class="input-group-append">
-                                    <div class="input-group-text"><i class="now-ui-icons ui-1_zoom-bold"></i> 
+                                    <div class="input-group-text"><i class="now-ui-icons ui-1_zoom-bold"></i>
                                     </div>
                                 </div>
                             </div>
@@ -110,13 +132,13 @@ if (isset($_GET['action']) && !empty($_GET['action']))
                         <ul class="navbar-nav">
                             <li class="nav-item">
                                 <a class="nav-link" href="#pablo"> <i class="now-ui-icons media-2_sound-wave"></i>
-                                    <p> <span class="d-lg-none d-md-block">Stats</span> </p>  
+                                    <p> <span class="d-lg-none d-md-block">Stats</span> </p>
                                 </a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <i class="now-ui-icons location_world"></i>
-                                    <p> <span class="d-lg-none d-md-block">Some Actions</span> </p>                        
+                                    <p> <span class="d-lg-none d-md-block">Some Actions</span> </p>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
                                     <a class="dropdown-item" href="#">Action</a>
@@ -137,21 +159,48 @@ if (isset($_GET['action']) && !empty($_GET['action']))
             </nav>
             <div class="panel-header panel-header-sm">
             </div>
+            <div id="id01">
 
-            <div class="content">
+
+                <div class="content">
+
+                    <div class="row">
+                        <div class=" col-md-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h5 class="card-category"></h5>
+                                    <h4 class="card-title">Companies</h4>
+                                </div>
+                                <div class="card-body">
+
+                                    <!----CARD HERE: echo output function from view classes-->
+                                    <?php
+                                    echo $view->output();
+                                    ?>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="content" id="id02" style="display: None">
 
                 <div class="row">
-                    <div class=" col-md-12">    
+                    <div class=" col-md-10">
                         <div class="card">
                             <div class="card-header">
-                              <h5 class="card-category"></h5>
-                              <h4 class="card-title">Companies</h4>
+                                <h5 class="card-category"></h5>
+                                <h4 class="card-title">Company Profile</h4>
                             </div>
                             <div class="card-body">
-                    
+
                                 <!----CARD HERE: echo output function from view classes-->
                                 <?php
-                                    echo $view->output();
+                                echo $view2->editCard();
                                 ?>
 
 
@@ -160,23 +209,48 @@ if (isset($_GET['action']) && !empty($_GET['action']))
                     </div>
                 </div>
             </div>
+
+            <div class="content" id="id03" style="display: None">
+
+                <div class="row">
+                    <div class=" col-md-10">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-category"></h5>
+                                <h4 class="card-title">Company Profile</h4>
+                            </div>
+                            <div class="card-body">
+
+                                <!----CARD HERE: echo output function from view classes-->
+                                <?php
+                                echo $view2->output();
+                                ?>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
+    </div>
 
 
 
-        <!--   Core JS Files   -->
-        <script src="../assets/js/core/jquery.min.js"></script>
-        <script src="../assets/js/core/popper.min.js"></script>
-        <script src="../assets/js/core/bootstrap.min.js"></script>
-        <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-        <!--  Google Maps Plugin    -->
-        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-        <!-- Chart JS -->
-        <script src="../assets/js/plugins/chartjs.min.js"></script>
-        <!--  Notifications Plugin    -->
-        <script src="../assets/js/plugins/bootstrap-notify.js"></script>
-        <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-        <script src="../assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script>
+    <!--   Core JS Files   -->
+    <script src="../assets/js/core/jquery.min.js"></script>
+    <script src="../assets/js/core/popper.min.js"></script>
+    <script src="../assets/js/core/bootstrap.min.js"></script>
+    <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+    <!--  Google Maps Plugin    -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+    <!-- Chart JS -->
+    <script src="../assets/js/plugins/chartjs.min.js"></script>
+    <!--  Notifications Plugin    -->
+    <script src="../assets/js/plugins/bootstrap-notify.js"></script>
+    <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="../assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script>
 </body>
 
 </html>

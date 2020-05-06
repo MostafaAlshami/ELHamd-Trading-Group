5c7a99ca682afe79fedc45e399e9c3d7d7a2351e
aya-fathy <aya-fathy@users.noreply.github.com>
2020-05-06 14:55:57 +0200

    Phase 1(backend)

--- sw.sql
+++ sw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2020 at 04:23 AM
+-- Generation Time: May 06, 2020 at 01:18 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.1.32
 
@@ -25,28 +25,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `address`
+-- Table structure for table `bankaccount`
 --
 
-CREATE TABLE `address` (
-  `address_id` int(6) UNSIGNED NOT NULL,
-  `country` varchar(20) DEFAULT NULL,
-  `city` varchar(20) DEFAULT NULL,
-  `district` varchar(20) DEFAULT NULL,
-  `street` varchar(20) DEFAULT NULL,
-  `building` varchar(20) DEFAULT NULL,
-  `postcode` varchar(20) DEFAULT NULL
+CREATE TABLE `bankaccount` (
+  `ID` int(11) NOT NULL,
+  `bank_id` int(11) NOT NULL,
+  `address_id` int(11) NOT NULL,
+  `swift_code` int(11) NOT NULL,
+  `account_no` int(11) NOT NULL,
+  `fax` int(11) NOT NULL,
+  `benef_name` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `address`
---
-
-INSERT INTO `address` (`address_id`, `country`, `city`, `district`, `street`, `building`, `postcode`) VALUES
-(1, 'Egypt', 'Cairo', 'Fifth', '21', '8', '81242'),
-(2, 'Egypt', 'Cairo', 'third', '81', '15', '24242'),
-(3, 'Indonesia', 'Bali', NULL, '77', '9', '47829'),
-(4, 'Egypt', 'Alex', NULL, '5', '2', '6589');
 
 -- --------------------------------------------------------
 
@@ -60,61 +50,38 @@ CREATE TABLE `company` (
   `email` varchar(50) DEFAULT NULL,
   `url` varchar(50) DEFAULT NULL,
   `phoneNumber` varchar(30) DEFAULT NULL,
-  `type` varchar(20) DEFAULT NULL,
-  `addressID` int(6) UNSIGNED NOT NULL
+  `address` varchar(30) NOT NULL,
+  `type` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `company`
 --
 
-INSERT INTO `company` (`company_id`, `company_name`, `email`, `url`, `phoneNumber`, `type`, `addressID`) VALUES
-(1, 'Elhamd', 'elhamd@gmail.com', 'www.elhamdTrading.com', '017767678', 'Trading', 1),
-(2, 'Abu-Auf', 'abuauf@hotmail.com', 'www.abuauf.com', '08738634627', 'Trading', 2),
-(3, 'CRM', 'crm@hotmail.com', 'www.crm.com', '01176767878', NULL, 4);
+INSERT INTO `company` (`company_id`, `company_name`, `email`, `url`, `phoneNumber`, `address`, `type`) VALUES
+(1, 'Elhamd', 'elhamd@gmail.com', 'www.elhamdTrading.com', '017767678', 'Cairo', 'Trading'),
+(2, 'Abu-Auf', 'abuauf@hotmail.com', 'www.abuauf.com', '08738634627', '', 'Trading'),
+(3, 'CRM', 'crm@hotmail.com', 'www.crm.com', '01176767878', '', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `address`
---
-ALTER TABLE `address`
-  ADD PRIMARY KEY (`address_id`);
-
---
 -- Indexes for table `company`
 --
 ALTER TABLE `company`
-  ADD PRIMARY KEY (`company_id`),
-  ADD KEY `addressID` (`addressID`);
+  ADD PRIMARY KEY (`company_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `address`
---
-ALTER TABLE `address`
-  MODIFY `address_id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
   MODIFY `company_id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `company`
---
-ALTER TABLE `company`
-  ADD CONSTRAINT `company_ibfk_1` FOREIGN KEY (`addressID`) REFERENCES `address` (`address_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

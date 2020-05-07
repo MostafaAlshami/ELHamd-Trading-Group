25bad9a9c06b220d8e1b0a0ca105fb0b84ee865b
OmarAttia98 <omar.attia98@gmail.com>
2020-05-07 04:36:10 +0200

    Added Storage mazboot

--- app/model/storage_model.php
+++ app/model/storage_model.php
@@ -7,128 +7,118 @@
 class Storage extends Model 
 {
   private $SID;
-  private $product_ID;
+  private $product_name;
+  private $product_id;
   private $quantity;
- 
 
-  function __construct($SID, $product_ID="", $quantity="")
+
+   function __construct($SID, $product_name="", $product_id="", $quantity="" )
   {
     $this->SID = $SID;   
     $this->db = $this->connect();
 
-    if("" === $product_ID)
+    if(""===$SID)
     {
       $this->readStorage($SID);
     }
     else
     {
-      $this->product_ID = $product_ID;
+      $this->product_id = $product_id;
+      $this->product_name = $product_name;
       $this->quantity = $quantity;
     }
   }
-
-  function getStorageID() {
-    return $this->SID; 
-  }
-  function setStorageID($SID) {
-    return $this->SID = $SID;
+  
+  public function getSID()
+  {
+    return $this->SID;
   }
 
-  function getProduct_ID() {
-    return $this->product_ID; 
-  }
-  function setProduct_ID($product_ID) {
-    return $this->product_ID = $product_ID;
+
+  public function setSID($SID)
+  {
+    $this->SID = $SID;
+
+    return $this;
   }
 
-  function getquantity() {
-    return $this->quantity; 
-  }
-  function setquantity($quantity) {
-    return $this->quantity = $quantity;
+  public function getproduct_id()
+  {
+    return $this->product_id;
   }
 
+ 
+  public function setproduct_id($product_id)
+  {
+    $this->product_id = $product_id;
+
+    return $this;
+  }
+  
+
+  public function getquantity()
+  {
+    return $this->quantity;
+  }
+
+  
+  public function setquantity($quantity)
+  {
+    $this->quantity = $quantity;
+
+    return $this;
+  }
+/**
+   * Get the value of product_name
+   */ 
+  public function getProduct_name()
+  {
+    return $this->product_name;
+  }
+
+  /**
+   * Set the value of product_name
+   *
+   * @return  self
+   */ 
+  public function setProduct_name($product_name)
+  {
+    $this->product_name = $product_name;
+
+    return $this;
+  }
+  
+ 
+  
   function readStorage($SID)
   {
-    $sql = "SELECT * FROM storage where ID =" .$SID;
+    $sql = "SELECT * FROM storage";
     $db = $this->connect();
-    $result = $db->query($sql);
-
+    $result = $db->query($sql); 
     if($result->num_rows == 1)
     {
       $row = $db->fetchRow();
-      $this->product_ID = $row["product_ID"];
+      $this->product_id = $row["product_id"];
+      $this->product_name = $row["productname"];
       $this->quantity = $row["quantity"];
     }
     else
     {
-      $this->product_ID = "";
+      
+      $this->product_name = "";
+      $this->product_id = "";
       $this->quantity = "";
     }
   }
 
-  function insertStorage($SID, $product_ID, $quantity, $district, $street, $building, $postcode)
-  {
-    $product_ID = $this->dbh->getConn()->real_escape_string($product_ID);
-    $quantity = $this->dbh->getConn()->real_escape_string($quantity);
-
-    $sql = "INSERT INTO storage (product_ID, quantity, district, street, building, postcode) 
-            VALUES ('$product_ID', '$quantity', '$district', '$street', '$building', '$postcode')";
-       
-     ///CHECK echos
-    if($this->dbh->query($sql) === true)
-    {
-      echo "Records inserted successfully.";
-    }
-    else
-    {  
-      echo "ERROR: Could not execute $sql. " . $conn->error;
-    }
-    //FIX AND TEST
-    //array_push($this->fruits, new Fruit("0","test","1.0"));
-    }
-  }
-
-
-  function editStorage($SID, $product_ID, $quantity, $district, $street, $building, $postcode)
-  {
-    $product_ID = $this->dbh->getConn()->real_escape_string($product_ID);
-    $quantity = $this->dbh->getConn()->real_escape_string($quantity);
-    
-
-   $sql = "UPDATE storage SET product_ID = '$product_ID', quantity = '$quantity' WHERE ID = $SID"; 
-
-    ///CHECK echos
-    if($this->dbh->query($sql) === true)
-    {
-      echo "Records updated successfully.";
-    }
-    else
-    {  
-      echo "ERROR: Could not execute $sql. " . $conn->error;
-    }
-    }
-
-
-  function deleteStorage($SID)
-  {
-    $sql="DELETE FROM storage WHERE ID = $SID";
-       
-    ///CHECK echos
-    if($this->dbh->query($sql) === true)
-    {
-      echo "Record deleted successfully.";
-    } 
-    else
-    {
-    echo "ERROR: Could not execute $sql. " . $conn->error;
-    }
-  }
-
-
-      
-
-
-
 }
+
+
+
+  
+  
+
+  
+
+
 ?>    
--- app/view/storage_view.php
+++ app/view/storage_view.php
@@ -4,18 +4,46 @@ require_once(__ROOT__ . "view/view.php");
 
 class ViewStorage extends View{	
 	public function output(){
-		$str = "";
-		$str.="<a href='profile.php'>Back to Profile </a>";
-		$str.="<table>";
-		$str.="<tr><th>Name</th><th>Year</th></tr>";
-		foreach($this->model->getStorage() as $Storage){
-			$str.="<tr>";
-			$str.="<td>".$Storage->getStorageID() ."  </td> ";
-			$str.="<td>" . $Storage->getProduct_ID() ."</td> ";
-			$str.="<td>" . $Storage->getquantity()() ."</td> ";
-			$str.="</tr>";
+$str="";
+		foreach($this->model->getStorages() as $Storage){
+			
+			$str.="<div class='table-responsive'>";
+            $str.="<table class='table'>";
+			$str.="<thead class='text-primary'>";
+			$str.="<th>Product ID</th>";
+			$str.="<th><input type='String' class='form-control' placeholder='".$Storage->getProduct_name() ."' > </th>";  
+			$str.="<th>Product Name</th>";
+            $str.="<th><input type='String' class='form-control' placeholder='".$Storage->getproduct_id() ."' > </th>";  
+            $str.="<th></th><th></th>";
+            $str.="<th>Quantity</th>";
+            $str.="<th><input type='number' class='form-control' placeholder='". $Storage->getquantity() ."'></th>";
+            $str.="</thead>";
+
+            $str.="<tbody>";
+            $str.="<tr>";
+            $str.="    <th>Date</th>";
+            $str.= "<td><input type='date' class='form-control' placeholder=''></td>  ";                                     
+            $str.=" <th>Quantity In</th>";
+            $str.= "<td><input type='number' class='form-control' placeholder=''></td>";
+            $str.= " <th>Quantity Out</th>";
+            $str.= " <td><input type='number' class='form-control' placeholder=''></td>";
+            $str.= " <th>Report</th>";
+            $str.= " <td><input type='String' class='form-control' placeholder=''></td>";
+            $str.="</tr>";
+            $str.=" <tr>";
+            $str.="    <td><button class=' btn'>Save</button></td><td></td>";
+            $str.="  <td></td> <td></td>";
+            $str.="  <td></td> <td></td>";
+            $str.="  <td></td> <td></td>";
+            $str.="  </tr>";
+            $str.=" </tbody>";
+            $str.=" </table>";
+            $str.="</div>";
+			"<tr>";
 		}
+		return $str;	
 	}
+
 }
 
 

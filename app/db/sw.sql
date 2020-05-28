4ce17c61cf58b3b02987e74a735d5cfbfcc91823
aya-fathy <aya-fathy@users.noreply.github.com>
2020-05-28 20:43:47 +0200

    Re-arranged folders. added tables to database

--- app/db/sw.sql
+++ app/db/sw.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2020 at 07:51 PM
+-- Generation Time: May 28, 2020 at 07:41 PM
 -- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.11
+-- PHP Version: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,8 +19,56 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sw2`
+-- Database: `sw`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bankacc`
+--
+
+CREATE TABLE `bankacc` (
+  `acc_id` int(8) NOT NULL,
+  `bank_name` varchar(30) NOT NULL,
+  `bank_branch` varchar(30) NOT NULL,
+  `benef_name` varchar(30) NOT NULL,
+  `acc_no` varchar(30) NOT NULL,
+  `swift_code` varchar(30) NOT NULL,
+  `fax` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `phone_no` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commission`
+--
+
+CREATE TABLE `commission` (
+  `contract_no` varchar(20) NOT NULL,
+  `discharge_port` varchar(30) NOT NULL,
+  `landing_port` varchar(30) NOT NULL,
+  `intermediary_acc` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commodity`
+--
+
+CREATE TABLE `commodity` (
+  `comm_id` int(11) NOT NULL,
+  `contract_no` varchar(20) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` float NOT NULL,
+  `unit_price` float NOT NULL,
+  `total_price` double NOT NULL,
+  `package_weight` float NOT NULL,
+  `package_type` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -48,6 +96,24 @@ INSERT INTO `company` (`company_id`, `company_name`, `email`, `curl`, `phoneNumb
 (3, 'CRM', 'crm@hotmail.com', 'www.crm.com', '01176767878', 'Cairo', 'shipping'),
 (4, 'CRM', 'crm@gmail.vom', 'www.crm.com', '01776767845', 'golf city', 'trading'),
 (5, 'Beansco', 'beansco@gmail.com', 'www.beansc.com', '01776767845', 'golf city', 'trading');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contract`
+--
+
+CREATE TABLE `contract` (
+  `contract_no` varchar(20) NOT NULL,
+  `contract_date` date NOT NULL,
+  `seller_id` int(6) NOT NULL,
+  `buyer_id` int(6) NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `bankAcc_id` int(11) NOT NULL,
+  `total_quantity` float NOT NULL,
+  `shipment_date` date NOT NULL,
+  `type` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -82,6 +148,33 @@ INSERT INTO `employee` (`ID`, `First_Name`, `Last_Name`, `email`, `address`, `De
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `import`
+--
+
+CREATE TABLE `import` (
+  `contract_no` varchar(20) NOT NULL,
+  `origin` varchar(30) NOT NULL,
+  `destination` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `invoice_no` int(11) NOT NULL,
+  `invoice_date` date NOT NULL,
+  `contract_no` varchar(20) NOT NULL,
+  `final_cost` float NOT NULL,
+  `payemnt_terms` text NOT NULL,
+  `remarks` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
@@ -102,6 +195,40 @@ INSERT INTO `product` (`ID`, `name`, `description`, `pic`, `origin`) VALUES
 (30, 'Cashew', 'cajew', 'https://image.shutterstock.com/image-photo/roasted-cashew-nuts-green-leaves-260nw-1239793840.jpg', 'Sudan'),
 (40, 'Potato', 'btates', 'https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/02/raw-potatoes-potato-1296x728-header.jpg?w=1155&h=1528', 'Egypt'),
 (45, 'Peanuts', 'fosdo2', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/peanuts-royalty-free-image-616003590-1532549624.jpg', 'Russia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipment`
+--
+
+CREATE TABLE `shipment` (
+  `shipment_id` int(11) NOT NULL,
+  `contract_no` varchar(20) NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `booking_id` int(11) NOT NULL,
+  `arrival_date` date NOT NULL,
+  `invoice_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shippingbooking`
+--
+
+CREATE TABLE `shippingbooking` (
+  `shipping_id` int(11) NOT NULL,
+  `shippingLine` int(6) NOT NULL,
+  `contract_no` varchar(20) NOT NULL,
+  `shipping_date` date NOT NULL,
+  `landing_port` varchar(30) NOT NULL,
+  `discharge_port` varchar(30) NOT NULL,
+  `container_no` varchar(11) NOT NULL,
+  `ship_name` varchar(30) NOT NULL,
+  `trip_no` varchar(11) NOT NULL,
+  `billOf_landing` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -149,12 +276,29 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`ID`, `username`, `password`, `type`, `emp_ID`) VALUES
 (126, 'omar', '$2y$10$Tz3320XG.dZmKlaFhdNcae3qPvA63UtA0FnKuXN6taoH8TG87eMBS', 0, 126),
-(130, 'Mostafa', '$2y$10$Tz3320XG.dZmKlaFhdNcae3qPvA63UtA0FnKuXN6taoH8TG87eMBS', 0, 130),
-(131, 'Aya', '$2y$10$Tz3320XG.dZmKlaFhdNcae3qPvA63UtA0FnKuXN6taoH8TG87eMBS', 0, 131);
+(130, 'Mostafa', '$2y$10$Tz3320XG.dZmKlaFhdNcae3qPvA63UtA0FnKuXN6taoH8TG87eMBS', 0, 130);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bankacc`
+--
+ALTER TABLE `bankacc`
+  ADD PRIMARY KEY (`acc_id`);
+
+--
+-- Indexes for table `commission`
+--
+ALTER TABLE `commission`
+  ADD PRIMARY KEY (`contract_no`);
+
+--
+-- Indexes for table `commodity`
+--
+ALTER TABLE `commodity`
+  ADD PRIMARY KEY (`comm_id`);
 
 --
 -- Indexes for table `company`
@@ -163,10 +307,28 @@ ALTER TABLE `company`
   ADD PRIMARY KEY (`company_id`);
 
 --
+-- Indexes for table `contract`
+--
+ALTER TABLE `contract`
+  ADD PRIMARY KEY (`contract_no`);
+
+--
 -- Indexes for table `employee`
 --
 ALTER TABLE `employee`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `import`
+--
+ALTER TABLE `import`
+  ADD PRIMARY KEY (`contract_no`);
+
+--
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`invoice_no`);
 
 --
 -- Indexes for table `product`
@@ -174,6 +336,18 @@ ALTER TABLE `employee`
 ALTER TABLE `product`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `shipment`
+--
+ALTER TABLE `shipment`
+  ADD PRIMARY KEY (`shipment_id`);
+
+--
+-- Indexes for table `shippingbooking`
+--
+ALTER TABLE `shippingbooking`
+  ADD PRIMARY KEY (`shipping_id`);
 
 --
 -- Indexes for table `storage`
@@ -194,6 +368,18 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `bankacc`
+--
+ALTER TABLE `bankacc`
+  MODIFY `acc_id` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `commodity`
+--
+ALTER TABLE `commodity`
+  MODIFY `comm_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `company`
@@ -217,13 +403,13 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `storage`
 --
 ALTER TABLE `storage`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=131;
 
 --
 -- Constraints for dumped tables

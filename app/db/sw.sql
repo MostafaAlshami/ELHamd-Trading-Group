bfb228d7aa1a40ebc4e5b230c5971b3e6fe534c1
aya-fathy <aya-fathy@users.noreply.github.com>
2020-06-13 18:25:31 +0200

    database

--- app/db/sw.sql
+++ app/db/sw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 13, 2020 at 05:51 PM
+-- Generation Time: Jun 13, 2020 at 06:22 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.1.32
 
